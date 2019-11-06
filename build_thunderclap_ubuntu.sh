b9d93b70033cdbc705a686568bbf85d0c0e15c3e
Theo Markettos <atm26@cl.cam.ac.uk>
2019-11-06 15:25:10 +0000

    For now, comment out multi-build things

--- build_thunderclap_ubuntu.sh
+++ build_thunderclap_ubuntu.sh
@@ -24,17 +24,17 @@ rm -rf mnt
 
 # copy in the thunderclap binary to the image
 mkdir -p payload/root
-mv build-arm/thunderclap payload/root/thunderclap
+#mv build-arm/thunderclap payload/root/thunderclap
 
 # build the SD card and compress it
-if [ "$FPGA" = "enclustra-mercury-aa1-pe1" ] ; then
+#if [ "$FPGA" = "enclustra-mercury-aa1-pe1" ] ; then
 	./scripts/build_ubuntu_sdcard.sh  boards/enclustra-mercury-aa1-pe1 refdes system payload libpixman-1-0 && \
 	pxz sdimage.img
-else
-	./scripts/build_ubuntu_sdcard.sh  boards/intel-a10soc-devkit ghrd_10as066n2 ghrd_10as066n2 payload libpixman-1-0 && \
-	pxz sdimage.img
-
-fi
+#else
+#	./scripts/build_ubuntu_sdcard.sh  boards/intel-a10soc-devkit ghrd_10as066n2 ghrd_10as066n2 payload libpixman-1-0 && \
+#	pxz sdimage.img
+#
+#fi
 
 # make sure everything is unmounted
 umount_multiple mnt/1
